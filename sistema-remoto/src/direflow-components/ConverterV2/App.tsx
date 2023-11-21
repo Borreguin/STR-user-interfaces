1ccb3ff61ab26a90b64aa932d8971e31954e5237
Pablo Sanchez <pablonex77@gmail.com>
2023-11-20 20:57:52 -0500

    Fix download excel method v2

--- sistema-remoto/src/direflow-components/ConverterV2/App.tsx
+++ sistema-remoto/src/direflow-components/ConverterV2/App.tsx
@@ -19,13 +19,11 @@ const ConverterV2: FC<ConverterV2Props> = () => {
       formData.append("excel_file", file);
       req.open("POST", UPLOAD_ENDPOINT);
       req.send(formData);
-
+      req.responseType = 'blob'
       req.onload = () => {
-        var blobText = req.responseText;
-        var blob = new Blob([blobText], { type: 'text/plain' });
-
-        var contentDispo = req.getResponseHeader('Content-Disposition');
-        var fileName = contentDispo.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1];
+        const blob = req.response;
+        const contentDispo = req.getResponseHeader('Content-Disposition');
+        const fileName = contentDispo.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1];
         saveBlob(blob, fileName);
         if(inputRef){
           inputRef.current.value = null;
@@ -40,21 +38,21 @@ const ConverterV2: FC<ConverterV2Props> = () => {
   };
 
   function saveBlob(blob: Blob, fileName: string) {
-    var a = document.createElement('a');
+    const a = document.createElement('a');
     a.href = window.URL.createObjectURL(blob);
     a.download = fileName;
     a.dispatchEvent(new MouseEvent('click'));
   }
 
   return (
-    <div class="w-50">
+    <div className="w-50">
       <form onSubmit={handleSubmit}>
-        <div class="custom-file">
-          <input class="form-control" ref={inputRef} onChange={(e) => setFile(e.target.files[0])} type="file" id="formFile" />
+        <div className="custom-file">
+          <input className="form-control" ref={inputRef} onChange={(e) => setFile(e.target.files[0])} type="file" id="formFile" />
         </div>
         <br/>
         <br/>
-        <button type='submit' class="btn btn-primary" disabled={!(file)}>
+        <button type='submit' className="btn btn-primary" disabled={!(file)}>
           Convertir
         </button>
       </form>

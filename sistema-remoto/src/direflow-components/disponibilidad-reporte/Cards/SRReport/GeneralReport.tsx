de507fa744d219313a4449d8dfc12f5920567546
RobertoSanchezFormos <roberto.sanchez@formos.com>
2024-01-18 09:10:39 -0500

    Reporte disponibilidad

--- sistema-remoto/src/direflow-components/disponibilidad-reporte/Cards/SRReport/GeneralReport.tsx
+++ sistema-remoto/src/direflow-components/disponibilidad-reporte/Cards/SRReport/GeneralReport.tsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from "react-bootstrap";
 import "./style.css";
 import { Report } from "./Report";
+import { RowValue } from "./RowValue";
 
 export interface GeneralReportProps {
   report: Report;
@@ -10,75 +11,136 @@ export interface GeneralReportProps {
 }
 
 class SRGeneralReport extends Component<GeneralReportProps> {
-
-  constructor(props) { 
-    super(props)
+  constructor(props) {
+    super(props);
     let tiempo_calculo_segundos = 0;
-    this.props.report.reportes_nodos.forEach((report) => { 
-      tiempo_calculo_segundos = Math.max(tiempo_calculo_segundos, report.tiempo_calculo_segundos);
-    })
+    this.props.report.reportes_nodos.forEach((report) => {
+      tiempo_calculo_segundos = Math.max(
+        tiempo_calculo_segundos,
+        report.tiempo_calculo_segundos,
+      );
+    });
   }
 
-  _format_date = () => { 
+  _format_date = () => {
     return this.props.report.actualizado.split(".")[0];
-  }
+  };
 
-  _format_percentage = (percentage) => { 
+  _format_percentage = (percentage) => {
     if (percentage < 0) {
       return "No definida";
-    } else { 
+    } else {
       return percentage.toFixed(6);
     }
-  }
+  };
 
   render() {
-    return (
-       this.props.report.procesamiento === undefined? <></>:
+    return this.props.report.procesamiento === undefined ? (
+      <></>
+    ) : (
       <div className="gr-sc-container">
         <div className="gr-sc-result">
-          <Card className="gr-sc-block" border="dark" >
+          <Card className="gr-sc-block" border="dark">
             <Card.Body className="gr-sc-padding">
-            <Card.Title>Resultado</Card.Title>
-              <div className="gr-sc-subtitle">Disponibilidad Promedio:</div>
-              <div className="gr-sc-value">{this._format_percentage(this.props.report.disponibilidad_promedio_porcentage)}%</div>
-              <div className="gr-sc-subtitle">Disponibilidad Promedio Ponderada:</div>
-                <div className="gr-sc-value">{this._format_percentage(this.props.report.disponibilidad_promedio_ponderada_porcentage)}%</div>
+              <Card.Title>Resultado</Card.Title>
+              <div>
+                <RowValue
+                  label={"Disponibilidad Promedio:"}
+                  value={
+                    this._format_percentage(
+                      this.props.report.disponibilidad_promedio_porcentage,
+                    ) + "%"
+                  }
+                />
+                <RowValue
+                  label={"Disponibilidad Promedio Ponderada:"}
+                  value={
+                    this._format_percentage(
+                      this.props.report
+                        .disponibilidad_promedio_ponderada_porcentage,
+                    ) + "%"
+                  }
+                />
                 <br></br>
-                <div className="gr-sc-subtitle">Fecha de cálculo</div>
-                <div className="gr-sc-date">{this._format_date()}</div>
-              <div className="gr-sc-footer">Periodo de evaluación: {this.props.report.periodo_evaluacion_minutos} minutos</div>
+                <RowValue
+                  label={"Fecha de cálculo:"}
+                  value={this._format_date()}
+                  valueClassName="gr-sc-date"
+                />
+                <br></br>
+              </div>
+              <div className="gr-sc-footer">
+                Periodo de evaluación:{" "}
+                {this.props.report.periodo_evaluacion_minutos} minutos
+              </div>
             </Card.Body>
           </Card>
         </div>
         <div className="gr-sc-processing">
-          <Card className="gr-sc-block" border="dark" >
+          <Card className="gr-sc-block" border="dark">
             <Card.Body className="gr-sc-padding">
               <Card.Title>Procesamiento</Card.Title>
-              <div className="gr-sc-process-description">Tags calculadas:</div>
-              <div className="gr-sc-process-value">{this.props.report.procesamiento.numero_tags_total}</div>
-              <div className="gr-sc-process-description">UTRs calculadas:</div>
-              <div className="gr-sc-process-value">{this.props.report.procesamiento.numero_utrs_procesadas}</div>
-              <div className="gr-sc-process-description">Entidades calculadas:</div>
-              <div className="gr-sc-process-value">{this.props.report.procesamiento.numero_entidades_procesadas}</div>
-              <div className="gr-sc-process-description">Nodos calculados:</div>
-              <div className="gr-sc-process-value">{this.props.report.procesamiento.numero_nodos_procesados}</div>
-              <div className="gr-sc-footer">Tiempo de procesamiento: {Math.floor(this.props.report.tiempo_calculo_segundos/60)} min {Math.floor(this.props.report.tiempo_calculo_segundos % 60)} seg. </div>
+              <div>
+                <RowValue
+                  label={"Tags calculadas:"}
+                  value={"" + this.props.report.procesamiento.numero_tags_total}
+                />
+                <RowValue
+                  label={"UTRs calculadas:"}
+                  value={
+                    "" + this.props.report.procesamiento.numero_utrs_procesadas
+                  }
+                />
+                <RowValue
+                  label={"Entidades calculadas:"}
+                  value={
+                    "" +
+                    this.props.report.procesamiento.numero_entidades_procesadas
+                  }
+                />
+                <RowValue
+                  label={"Nodos calculados:"}
+                  value={
+                    "" + this.props.report.procesamiento.numero_nodos_procesados
+                  }
+                />
+              </div>
+              <br></br>
+              <div className="gr-sc-footer">
+                Tiempo de procesamiento:{" "}
+                {Math.floor(this.props.report.tiempo_calculo_segundos / 60)} min{" "}
+                {Math.floor(this.props.report.tiempo_calculo_segundos % 60)}{" "}
+                seg.{" "}
+              </div>
             </Card.Body>
           </Card>
         </div>
         <div className="gr-sc-novedades">
-          <Card className="gr-sc-block" border="dark" >
+          <Card className="gr-sc-block" border="dark">
             <Card.Body className="gr-sc-padding">
-            <Card.Title>Novedades</Card.Title>
-            <div className="gr-sc-process-description">Tags falladas:</div>
-              <div className="gr-sc-process-value">{this.props.report.novedades.tags_fallidas}</div>
-              <div className="gr-sc-process-description">UTRs falladas:</div>
-              <div className="gr-sc-process-value">{this.props.report.novedades.utr_fallidas}</div>
-              <div className="gr-sc-process-description">Entidades falladas:</div>
-              <div className="gr-sc-process-value">{this.props.report.novedades.entidades_fallidas}</div>
-              <div className="gr-sc-process-description">Nodos fallados:</div>
-              <div className="gr-sc-process-value">{this.props.report.novedades.nodos_fallidos}</div>
-              <div className="gr-sc-footer">Click "novedades" para más información</div>
+              <Card.Title>Novedades</Card.Title>
+              <div>
+                <RowValue
+                  label={"Tags falladas:"}
+                  value={"" + this.props.report.novedades.tags_fallidas}
+                />
+                <RowValue
+                  label={"UTRs falladas:"}
+                  value={"" + this.props.report.novedades.utr_fallidas}
+                />
+                <RowValue
+                  label={"Entidades falladas:"}
+                  value={"" + this.props.report.novedades.entidades_fallidas}
+                />
+                <RowValue
+                  label={"Nodos fallados:"}
+                  value={"" + this.props.report.novedades.nodos_fallidos}
+                />
+              </div>
+              <br></br>
+              <div className="gr-sc-footer">
+                Click "novedades" para más información
+              </div>
             </Card.Body>
           </Card>
         </div>
---
+++ sistema-remoto/src/direflow-components/disponibilidad-reporte/Cards/SRReport/RowValue.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface RowValueProps {
+  label: string;
+  value: string;
+  valueClassName?: string;
+}
+
+export const RowValue = (props: RowValueProps) => {
+  const { label, value, valueClassName } = props;
+  console.log("valueClassName", valueClassName);
+  return (
+    <div className="gr-sc-row">
+      <div className="gr-sc-subtitle">{label}</div>
+      <div
+        className={
+          valueClassName !== undefined ? valueClassName : "gr-sc-value"
+        }
+      >
+        {value}
+      </div>
+    </div>
+  );
+};
